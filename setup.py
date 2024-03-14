a862b697b26119d633721fb9a09dbf0cae11ea15
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2024-03-14 01:00:15 -0300

    deploying to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lambda_forge",
-    version="1.0.11",
+    version="1.0.12",
     packages=find_packages(),
     license='MIT',
     install_requires=[
