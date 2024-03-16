495e27d0b07db747d2216faef8c130dba2b332ba
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2024-03-15 21:39:43 -0300

    framework working

--- lambda_forge/project_builder.py
+++ lambda_forge/project_builder.py
@@ -196,7 +196,235 @@ class ProdPipelineStack(cdk.Stack):
         )
 """
         return self
+    
+    def with_gitignore(self):
+        self.gitignore = """
+# Compiled source #
+###################
+*.swp
 
+# Packages #
+############
+# It's generally good practice to exclude package lock files to prevent conflicts between different environments
+package-lock.json
+
+# Python bytecode #
+###################
+__pycache__/
+*.py[cod]
+
+# Pytest cache #
+################
+.pytest_cache/
+
+# Virtual Environment #
+#######################
+.venv/
+venv/
+env/
+
+# Distribution / packaging #
+############################
+*.egg-info/
+dist/
+build/
+
+# PyInstaller #
+##############
+# Usually, you don't track these files but depends on the project
+# If you're using PyInstaller, uncomment the next line
+# *.spec
+
+# Installer logs #
+##################
+pip-log.txt
+pip-delete-this-directory.txt
+
+# Unit test / coverage reports #
+################################
+htmlcov/
+.coverage
+.coverage.*
+.coverage_e2e
+cov.xml
+coverage.xml
+nosetests.xml
+junit.xml
+
+# Translations #
+################
+*.mo
+*.pot
+
+# Django stuff: #
+#################
+*.log
+local_settings.py
+db.sqlite3
+db.sqlite3-journal
+
+# Flask stuff: #
+################
+instance/
+.webassets-cache
+
+# Scrapy stuff: #
+#################
+.scrapy
+
+# Sphinx documentation #
+########################
+docs/_build/
+
+# PyBuilder #
+#############
+target/
+
+# Jupyter Notebook #
+####################
+.ipynb_checkpoints
+
+# pyenv #
+#########
+.python-version
+
+# celery beat schedule file #
+#############################
+celerybeat-schedule
+
+# SageMath parsed files #
+#########################
+*.sage.py
+
+# dotenv #
+##########
+.env
+
+# Spyder project settings #
+###########################
+.spyderproject
+.spyproject
+
+# Rope project settings #
+#########################
+.ropeproject
+
+# mkdocs documentation #
+########################
+/site
+
+# mypy #
+########
+.mypy_cache/
+
+# Databases #
+#############
+*.db
+*.sql
+*.sqlite
+
+# Backup files #
+################
+*.bak
+
+# If you're using IntelliJ products (e.g., PyCharm), you might want to ignore these #
+#####################################################################################
+.idea/
+*.iml
+
+# Visual Studio Code #
+######################
+.vscode/
+
+# CDK asset staging directory #
+###############################
+.cdk.staging
+cdk.out
+
+# Misc #
+#######
+.DS_Store
+
+# Documentation files #
+#######################
+docs.yaml
+
+# Lambda Forge Stuff #
+###############
+.fc
+sam.out
+tested_endpoints.jsonl
+
+# Coverage reports #
+####################
+coverage.xml
+"""     
+        return self
+
+    def with_pre_commit(self):
+        self.pre_commit = """
+fail_fast: true
+
+repos:
+  - repo: local
+    hooks:
+      - id: cdk
+        name: remove cdk.out
+        entry: rm -rf cdk.out
+        language: system
+        pass_filenames: false
+        stages: [commit]
+      - id: unit_test
+        name: unit tests
+        entry: coverage run -m pytest -k 'unit.py'
+        language: system
+        pass_filenames: false
+        stages: [commit]
+      - id: coverage
+        name: coverage
+        entry: coverage report --fail-under=80
+        language: system
+        pass_filenames: false
+        stages: [commit]
+      - id: validate_docs
+        name: validate docs
+        entry: python validate_docs.py
+        language: system
+        pass_filenames: false
+        stages: [commit]
+      - id: generate_docs
+        name: generate docs
+        entry: python generate_docs.py
+        language: system
+        pass_filenames: false
+        stages: [commit]
+      - id: validate_integration
+        name: validate integration tests
+        entry: python validate_integration_tests.py
+        language: system
+        pass_filenames: false
+        stages: [commit]
+      - id: cleaning
+        name: cleaning
+        entry: rm -rf .fc coverage.xml .coverage tested_endpoints.jsonl htmlcov .DS_Store build/ gaia.egg-info/ docs.yaml
+        language: system
+        pass_filenames: false
+        stages: [commit]
+"""
+        return self
+    
+    def with_coverage(self):
+        self.coverage = """
+[run]
+branch = True
+include =
+    functions/**/main.py
+    functions/**/utils.py
+
+omit=
+"""
+        return self
+    
     def with_cdk(self, repo_owner, repo_name, bucket):
         cdk = {
             "app": "python3 app.py",
@@ -281,6 +509,9 @@ class ProdPipelineStack(cdk.Stack):
 
     def build(self):
         self.copy_folders("lambda_forge", "files", "")
+        self.make_file("", ".gitignore", self.gitignore)
+        self.make_file("", ".pre-commit-config.yaml", self.pre_commit)
+        self.make_file("", ".coveragerc", self.coverage)
 
         if self.dev:
             self.make_file(
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lambda_forge",
-    version="1.0.27",
+    version="1.0.33",
     packages=find_packages(),
     license="MIT",
     install_requires=[
