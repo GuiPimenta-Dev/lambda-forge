58e706c72ee98f2e73874bb52405a694c903eb2a
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2024-03-19 14:04:30 -0300

    test pipeline

--- lambda_forge/steps.py
+++ lambda_forge/steps.py
@@ -131,7 +131,7 @@ class Steps:
         )
 
     def validate_docs(self):
-        validate_docs = pkg_resources.resource_string(            __name__, "validate_docs.py"        )
+        validate_docs = pkg_resources.resource_string(__name__, "validate_docs.py")
         return pipelines.CodeBuildStep(
             "Validate Docs",
             input=self.source,
@@ -139,7 +139,7 @@ class Steps:
                 "pip install -r requirements.txt",
             ],
             commands=[
-                f"echo '{validate_docs.decode()}' > validate_integration_tests.py",
+                f"echo '{validate_docs.decode()}' > validate_docs.py",
                 "python validate_docs.py",
             ],
             build_environment=codebuild.BuildEnvironment(
