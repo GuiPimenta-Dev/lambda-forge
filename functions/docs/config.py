3a3323cc962020f8cbe4c306e22453a42b6bc4ef
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2024-03-21 14:22:09 -0300

    progress so far

--- functions/docs/config.py
+++
@@ -1,7 +0,0 @@
-
-from infra.services import Services
-
-class DocsConfig:
-
-    def __init__(self, services: Services) -> None:
-        services.api_gateway.create_docs(authorizer="docs_authorizer")
