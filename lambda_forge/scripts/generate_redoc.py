2c7d10d38a58f8db5a26026c55afe14f548f2cc2
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-04-24 12:18:51 -0300

    adding pipe docs

--- lambda_forge/scripts/generate_redoc.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-if __name__ == "__main__":
-    subprocess.run(["python", "generate_api_docs.py"], check=True)
-    subprocess.run(["redoc-cli", "bundle", "-o", "redoc.html", "docs.yaml"], check=True)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lambda_forge",
-    version="1.0.434",
+    version="1.0.440",
     packages=find_packages(),
     license="MIT",
     install_requires=[
