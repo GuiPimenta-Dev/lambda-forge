ab354ec22926b320eba7760eac84054e78e59f23
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-06-23 16:19:19 -0300

    updating version on setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="lambda_forge",
-    version="2.0.0",
+    version="2.0.2",
     packages=find_packages(),
     license="MIT",
     install_requires=[
