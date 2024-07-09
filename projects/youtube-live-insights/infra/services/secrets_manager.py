8df02c37519e02a247d6a9eba00bf735669c7888
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-07-09 19:39:45 -0300

    improving README.md

--- projects/youtube-live-insights/infra/services/secrets_manager.py
+++ projects/youtube-live-insights/infra/services/secrets_manager.py
@@ -15,11 +15,3 @@ class SecretsManager:
             id="AuthorizerSecret",
             secret_complete_arn="arn:aws:secretsmanager:us-east-2:211125768252:secret:youtube-live-insights-zgUYte",
         )
-
-
-{
-    "video_id": "559484e7-09b8-4dc7-a55c-cc4e0e810814",
-    "interval": 10,
-    "min_messages": 5,
-    "prompt": "Fernanda Kipper is a Brazilian content creator known for her insightful and educational videos on technology and programming. With a strong background in software development, she shares tutorials, tips, and career advice, helping her audience navigate the tech industry. Fernanda's channel is appreciated for its clear and approachable style, making complex topics accessible to beginners and experienced developers alike.",
-}
