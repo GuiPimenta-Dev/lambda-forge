3f6aa153e43bccd16f315d75b069c95336219166
kraanzu <kraanzu@gmail.com>
2024-08-13 11:47:16 +0530

    fix: id for cloudwatch_log

--- lambda_forge/logs/tui/ui/widgets/cloudwatch_log.py
+++ lambda_forge/logs/tui/ui/widgets/cloudwatch_log.py
@@ -1,5 +1,8 @@
 from textual.widget import Widget
+from ...api.forge_logs import LambdaGroup
 
 
 class CloudWatchLogs(Widget):
-    pass
+    def __init__(self, log_group: LambdaGroup):
+        self.log_group = log_group
+        super().__init__(id=log_group.name.replace("/", "-"))
