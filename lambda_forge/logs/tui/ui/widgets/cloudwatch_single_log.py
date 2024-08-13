9ce612aa25e2580e4658726a9061e9ec6abc2862
kraanzu <kraanzu@gmail.com>
2024-08-13 12:15:41 +0530

    better cloud watch single log

--- lambda_forge/logs/tui/ui/widgets/cloudwatch_single_log.py
+++ lambda_forge/logs/tui/ui/widgets/cloudwatch_single_log.py
@@ -1,4 +1,10 @@
 from textual.widgets.option_list import Option
+from ...api.forge_logs import CloudWatchLog
+
 
 class CloudWatchSingleLog(Option):
-    pass
+
+    def __init__(self, log: CloudWatchLog):
+        super().__init__("")
+        self.log = log
+        self.set_prompt(str(self.log.timestamp))
