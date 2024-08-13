18f3b3ea2f29d31edde662062cf0bee9f2c7ad5c
kraanzu <kraanzu@gmail.com>
2024-08-13 11:47:25 +0530

    use cloud watch widget

--- lambda_forge/logs/tui/ui/screens/index.py
+++ lambda_forge/logs/tui/ui/screens/index.py
@@ -1,10 +1,19 @@
 from textual.app import ComposeResult
 from textual.screen import Screen
-from textual.widgets import Static, TabbedContent, TabPane
+from textual.widgets import TabbedContent, TabPane
+from lambda_forge.logs.tui.api.forge_logs import ForgeLogsAPI
+from lambda_forge.logs.tui.ui.widgets.cloudwatch_log import CloudWatchLogs
 
 
 class Index(Screen):
+
+    @property
+    def logs_api(self) -> ForgeLogsAPI:
+        return self.app.logs_api
+
     def compose(self) -> ComposeResult:
         with TabbedContent():
-            with TabPane("CloudWatch Logs"):
-                yield Static()
+
+            for log_group in self.logs_api.get_lambdas():
+                with TabPane(log_group.group):
+                    yield CloudWatchLogs(log_group)
