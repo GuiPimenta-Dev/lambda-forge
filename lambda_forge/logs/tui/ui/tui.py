45d781049ae35a6e6fb31c718b2c502c233c0d48
kraanzu <kraanzu@gmail.com>
2024-08-16 14:28:06 +0530

    refactor screens

--- lambda_forge/logs/tui/ui/tui.py
+++ lambda_forge/logs/tui/ui/tui.py
@@ -5,10 +5,12 @@ from ..api import ForgeLogsAPI
 
 
 class ForgeLogsApp(App):
+    CSS_PATH = "styles.css"
+    SCREENS = {"index": Index()}
 
     def __init__(self, params: Optional[Dict]):
         super().__init__()
         self.logs_api = ForgeLogsAPI(params)
 
     def on_mount(self) -> None:
-        self.push_screen(Index())
+        self.push_screen("index")
