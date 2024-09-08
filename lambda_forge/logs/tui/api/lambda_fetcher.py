65a8992411e969fa040116c0f382e537bba39343
kraanzu <kraanzu@gmail.com>
2024-09-08 14:56:57 +0530

    fix: only allow lambda forge functions

--- lambda_forge/logs/tui/api/lambda_fetcher.py
+++ lambda_forge/logs/tui/api/lambda_fetcher.py
@@ -1,19 +1,9 @@
-import boto3
+import json
 
 
 def list_lambda_functions():
-    # Create a Lambda client
-    client = boto3.client("lambda")
-
-    # Initialize pagination
-    paginator = client.get_paginator("list_functions")
-    page_iterator = paginator.paginate()
-
-    # Iterate through all pages and collect function names
-    all_functions = []
-    for page in page_iterator:
-        functions = page["Functions"]
-        for function in functions:
-            all_functions.append(function["FunctionName"])
-
-    return all_functions
+    with open("functions.json") as f:
+        functions = json.load(f)
+        pre = "Lambda-Forge-Demo-"
+        function_names = [pre + i["name"] for i in functions]
+        return function_names
